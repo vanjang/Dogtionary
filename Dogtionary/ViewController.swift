0a4d45c26c44953cd4611a597478ebcdd25954a1
Myung Hoon So <joa214@hotmail.com>
2024-02-06 23:34:22 +0000

    Initial Commit

---
+++ Dogtionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dogtionary
+//
+//  Created by myung hoon on 06/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
